5d26245e082d09adfab355d2afb156fff93fd187
dhilton <dan@lightsandcolors.com>
2013-11-14 15:08:57 -0700

    IDLife initial commit: barebones rails 4 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IdlifeCom::Application.config.secret_key_base = '11a59adc176a9865d816948cc360fa93ff171ad1111afeb7b62175cd600f3fca99f3cf00f15e7862373fd80b4f2076f34955604a6909b63e54a35e177c2bb9ff'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IdlifeCom::Application.config.session_store :cookie_store, key: '_idlife_com_session'
