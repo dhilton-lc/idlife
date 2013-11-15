444440c3d0abd1a18da77d80b20345495779cefa
dhilton <dan@lightsandcolors.com>
2013-11-14 23:00:12 -0700

    Stub out additional actions in home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,24 @@
 class HomeController < ApplicationController
 
   def index
-
   end
 
   def faq
+  end
 
+  def terms
+  end
+
+  def privacy
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def comp_plan
   end
 
 end
